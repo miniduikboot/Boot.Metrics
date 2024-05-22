1fec8676d661909bd46e0faf3543d550b76a85f5
miniduikboot <mini@duikbo.at>
2024-05-22 21:47:06 +0200

    Add Github Actions

---
+++ build.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+set -euxo pipefail
+
+TAG=$(git describe --tags)
+LONG_TAG=$(git describe --tags --long --dirty --always)
+
+dotnet restore --locked-mode
+dotnet publish -c Release -p VersionPrefix="${TAG:1}"
+
+PACKDIR=$(mktemp -d)
+mkdir "$PACKDIR"/libraries
+mkdir "$PACKDIR"/plugins
+
+cp Boot.Metrics/bin/Release/net*/publish/Boot.Metrics.dll "$PACKDIR"/plugins
+cp Boot.Metrics/bin/Release/net*/publish/OpenTelemetry*.dll "$PACKDIR"/libraries
+
+pushd "$PACKDIR"
+zip -9r Boot.Metrics-"${LONG_TAG}".zip libraries plugins
+popd
+
+mkdir -p output
+cp "${PACKDIR}"/Boot.Metrics-"${LONG_TAG}".zip output
